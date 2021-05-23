52c7de06c67cec4dde4a412f84cd5f84dd354aec
James Park <jamesdparkny@gmail.com>
2021-05-23 10:49:47 -0400

    first commit

---
+++ VM/debug.h
@@ -0,0 +1,9 @@
+#ifndef Von_debug_h
+#define Von_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
