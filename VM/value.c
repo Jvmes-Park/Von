e23c3b92f8c28813d675c40a62fd1152d682bc41
James Park <jamesdparkny@gmail.com>
2021-06-03 23:41:39 -0400

    string print and equality support

--- VM/value.c
+++ VM/value.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include "object.h"
 #include "memory.h"
 #include "value.h"
 
@@ -33,6 +35,9 @@ void printValue(Value value) {
 			break;
 		case VAL_NUMBER:
 			printf("%g", AS_NUMBER(value));
+		case VAL_OBJ:
+			printObject(value);
+			break;
 	}
 }
 
@@ -46,6 +51,12 @@ vool valuesEqual(Value a, Value b) {
 			return true;
 		case VAL_NUMBER:
 			return AS_NUMBER(a) == AS_NUMBER(b);
+		case VAL_OBJ: {
+			ObjString* aString = AS_STRING(a);
+			ObjString* bString = AS_STRING(b);
+			return aString -> length == bString -> length && 
+				memcmp(aString -> chars, bString -> chars, aString -> length) == 0;	
+		     }
 		default:
 			return false;
 	}
