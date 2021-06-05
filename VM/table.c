b99e14ee82c5d6ac4fa87bab95aa85b180028912
James Park <jamesdparkny@gmail.com>
2021-06-05 00:08:38 -0400

    deleting entries

--- VM/table.c
+++ VM/table.c
@@ -21,13 +21,33 @@ void freeTable(Table* table) {
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
 	uint32_t index = key -> hash % capacity;
+	Entry* tombstone = NULL;
 	for (;;) {
 		Entry* entry = &entries[index];
-		if (entry -> key == key || entry -> key == NULL) {
-			return entry;
-		}
+		if (entry -> key == NULL) {
+			if (IS_NIL(entry -> value)) {
+				return tombstone != NULL ? tombstone : entry;
+			}
+			else {
+				if (tombstone == NULL)
+					tombstone = entry;
+			}
+			else if (entry -> key == key) {
+				return entry;
+			}
+		}	
 		index = (index + 1) % capacity;
 	}
+}
+
+bool tableGet(Table* table, ObjString* key, Value* value) {
+	if (table -> count == 0)
+		return false;
+	Entry* entry = findEntry(table -> entries, table -> capacity, key);
+	if (entry -> key == NULL)
+		return false;
+	*value = entry -> value;
+	return true;
 }
 
 static void adjustCapacity(Table* table, int capacity) {
@@ -36,6 +56,7 @@ static void adjustCapacity(Table* table, int capacity) {
 		entries[i].key = NULL;
 		entries[i].value = NIL_VAL;
 	}
+	table -> count = 0;
 	for (int i = 0; i < table -> capacity; i++) {
 		Entry* entry = &table -> entries[i];
 		if (entry -> key == NULL)
@@ -43,6 +64,7 @@ static void adjustCapacity(Table* table, int capacity) {
 		Entry* dest = findEntry(entries, capacity, entry -> key);
 		dest -> key = entry -> key;
 		dest -> value = entry -> value;
+		table -> count++;
 	}
 	FREE_ARRAY(Entry, table -> entries, table -> capacity);
 	table -> entries = entries;
@@ -56,11 +78,22 @@ bool tableSet(Table* table, ObjString* key, Value value) {
 	}
 	Entry* entry = findEntry(table -> entries, table -> capacity, key);
 	bool isNewKey = entry -> key == NULL;
-	if (isNewKey)
+	if (isNewKey && IS_NIL(entry -> value))
 		table -> count++;
 	entry -> key = key;
 	entry -> value = value;
 	return isNewKey;
+}
+
+bool tableDelete(Table* table, ObjString* key) {
+	if (table -> count == 0)
+		return false;
+	Entry* entry = findEntry(table -> entries, table -> capacity, key);
+	if (entry -> key == NULL)
+		return false;
+	entry -> key = NULL;
+	entry  -> value = BOOL_VAL(true);
+	return true;
 }
 
 void tableAddAll(Table* from, Table* to) {
--- VM/table.h
+++ VM/table.h
@@ -19,5 +19,7 @@ void initTable(Table* table);
 void freeTable(Table* table);
 void tableSet(Table* table, ObjString* key, Value value);
 void tableAddAll(Table* from, Table* to);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableDelete(Table* table, ObjString* key);
 
 #endif
