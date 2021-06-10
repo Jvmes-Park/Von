e1ffc3956ba4c67a8e05bcb1335b115d23556e6d
James Park <jamesdparkny@gmail.com>
2021-06-09 23:33:01 -0400

    function objects

--- VM/object.h
+++ VM/object.h
@@ -2,15 +2,19 @@
 #define Von_object_h
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)		(AS_OBJ(value) -> type)
 #define IS_STRING(value)	isObjType(value, OBJ_STRING)
+#define IS_FUNCTION(value)	isObjType(value, OBJ_FUNCTION)
 
 #define AS_STRING(value)	((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)	(((ObjString*)AS_OBJ(value)) -> chars)
+#define AS_FUNCTION(value)	(((ObjString*)AS_OBJ(value)))
 
 typedef enum {
+	OBJ_FUNCTION,
 	OBJ_STRING,
 } ObjType;
 
@@ -19,6 +23,13 @@ struct Obj {
 	struct Obj* next;
 };
 
+typedef struct {
+	Obj obj;
+	int arity;
+	Chunk chunk;
+	ObjString* name;
+} ObjFunction;
+
 struct ObjString {
 	Obj obj;
 	int length;
@@ -26,6 +37,7 @@ struct ObjString {
 	uint32_t hash;
 };
 
+ObjFunction* newFunction();
 ObjString* copyString(const char* chars, int length);
 
 void printObject(Value value);
