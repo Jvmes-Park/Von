676dee735b51ea441dce4dee89e80bff02d8de6b
James Park <jamesdparkny@gmail.com>
2021-06-13 21:51:13 -0400

    closing upvalues

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -43,6 +43,7 @@ typedef struct {
 typedef struct {
 	Token name;
 	int depth;
+	bool isCaptured;
 } Local;
 
 typedef struct {
@@ -195,6 +196,7 @@ static void initCompiler(Compiler* compiler, FunctionType type) {
 
 	Local* local = &current -> locals[current -> localCount++];
 	local -> depth = 0;
+	local -> isCaptured = false;
 	local -> name.start = "";
 	local -> name.length = 0;
 }
@@ -218,7 +220,12 @@ static void beginScope() {
 static void endScope() {
 	current -> scopeDepth--;
 	while (current -> localCount > 0 && current -> locals[current -> localCount - 1].depth > current -> scopeDepth) {
-		emitByte(OP_POP);
+		if (current -> locals[current -> localCount - 1].isCaptured) {
+			emitByte(OP_CLOSE_UPVALUE);
+		}
+		else {
+			emitByte(OP_POP);
+		}
 		current -> localCount--;
 	}
 }
@@ -296,6 +303,7 @@ static int resolveUpvalue(Compiler* compiler, Token* name) {
 		return -1;
 	int local = resolveLocal(compiler -> enclosing, name);
 	if (local != -1) {
+		compiler -> enclosing -> locals[local].isCaptured = true;
 		return addUpvalue(compiler, (uint8_t)local, true);
 	}
 
@@ -314,6 +322,7 @@ static void addLocal(Token name) {
 	Local* local = &current -> locals[current -> localCount++];
 	local -> name = name;
 	local -> depth = -1;
+	local -> isCaptured = false;
 }
 
 static void declareVariable() {
